aa106109254b82a9f258a3dba6253e36a8f4c93a
Yeudakimenka-Maksim <evdokimenko.maxim@gmail.com>
2016-07-18 21:20:16 +0300

    Completed task

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using PrimeGenerator;
+
+namespace ConsoleApplication
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var primes = PrimeSequenceGenerator.GenerateNFirst(4).ToList();
+
+            Console.Write("List: ");
+            primes.ForEach(prime => Console.Write(prime + " "));
+            Console.WriteLine();
+
+            var sum = primes.Sum(prime => (double)prime / (primes.IndexOf(prime) + 1));
+            Console.WriteLine("Sum = " + sum);
+
+            var result = Math.Floor(sum);
+            Console.WriteLine("Result = " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PrimeGenerator/PrimeSequenceGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeGenerator
+{
+    public static class PrimeSequenceGenerator
+    {
+        public static IEnumerable<int> GenerateNFirst(int n)
+        {
+            if (n < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(n), "Count 'n' must be greater than zero.");
+            }
+
+            var prime = 2;
+            yield return prime;
+
+            for (var i = 0; i < n - 1; i++)
+            {
+                prime = GenerateNext(prime);
+                yield return prime;
+            }
+        }
+
+        private static int GenerateNext(int prime)
+        {
+            for (var current = prime + 1; ; current++)
+            {
+                if (IsPrime(current))
+                {
+                    return current;
+                }
+            }
+        }
+
+        private static bool IsPrime(int number)
+        {
+            for (var denom = 2; denom * denom <= number; denom++)
+            {
+                if (number % denom == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+}
